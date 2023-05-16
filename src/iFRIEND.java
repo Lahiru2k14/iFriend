be50627a2135966067a132cc4bca06b11152ea30
lahiru2k14 <lahiru2k14@gmail.com>
2023-05-17 00:02:05 +0530

    12457

--- src/iFRIEND.java
+++ src/iFRIEND.java
@@ -1,6 +1,9 @@
 public class iFRIEND {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        String;
+
+        System.out.println();
+        System.out.println();
+
     }
 }
